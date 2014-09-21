e30d6edc55a5cf5a97e4451b48bdd0563d385bdb
Kenneth Ormandy <kenneth@chloi.io>
2014-09-21 01:39:55 -0700

    v0.1.0

---
+++ component.js
@@ -0,0 +1,14 @@
+{
+  "name": "big-ideas-text",
+  "repo": "kennethormandy/big-ideas-text",
+  "description": "Dynamically scale lines of text within a container.",
+  "version": "0.1.0",
+  "keywords": [
+    "type",
+    "typography"
+  ],
+  "license": "MIT",
+  "scripts": [
+    "dist/big-ideas-text.js"
+  ]
+}
